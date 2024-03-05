4761fd47067f78d3cb4035bbb0b504aa4967eeec
emma <223729@ids.upchiapas.edu.mx>
2024-03-05 09:01:30 -0600

    limites de peticiones

--- src/compras/infrestructure/routes/pedidos.router.ts
+++ src/compras/infrestructure/routes/pedidos.router.ts
@@ -1,8 +1,8 @@
 import express from  'express';
 import { PedidosController } from '../controller/pedidos.controller';
-
+import { accountLimiter } from '../../../peticiones/limit.peticiones';
 export const router = express.Router();
 
-router.post('/', PedidosController.createPedidos);
+router.post('/', accountLimiter, PedidosController.createPedidos);
 
 export default router;
